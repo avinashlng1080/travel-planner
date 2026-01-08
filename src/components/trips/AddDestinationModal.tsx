fdb08e944daac810602e83e21dd9c86b68e043ad
Avinash Lingaloo <avinashlng1080@gmail.com>
2026-01-08 06:11:16 +0400

    Update AddDestinationModal.tsx

--- src/components/trips/AddDestinationModal.tsx
+++ src/components/trips/AddDestinationModal.tsx
@@ -140,7 +140,8 @@ export default function AddDestinationModal({
       setShowPredictions(false);
     }
     setSelectedIndex(-1); // Reset selection when search changes
-  }, [searchQuery, search, clearPredictions]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchQuery]);
 
   // Scroll selected item into view
   useEffect(() => {
